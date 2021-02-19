1db5da5642b2ff9d34b065b5f81b55e4210fbe62
emakuhin <79087377+emakuhin@users.noreply.github.com>
2021-02-19 15:28:47 +0300

    Update core.py

--- core.py
+++ core.py
@@ -58,4 +58,4 @@ class Application:
             return [text.encode('utf-8')]
         else:
             start_response('404 NOT FOUND', [('Content-Type', 'text/html')])
-            return [b"Not Found"] 
+            return [b"Not Found"]
