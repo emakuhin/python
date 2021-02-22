be4c4a96e2d6fabb0a3b69ed47b20626339f1f17
e.makukhin <maku@mail.ru>
2021-02-22 15:52:45 +0300

    Lesson-4

--- views.py
+++ views.py
@@ -1,19 +1,21 @@
 from functions import render
+cousers=['Java', 'Python']
 def index(request):
     secret = request.get('secret_key', None)
     # Используем шаблонизатор
-    return '200 OK', render('index.html', secret=secret)
-def about(request):
+    courses = ['Java', 'Python']
+    return '200 OK', render('index.html', secret=secret, courses=courses)
+def teachers(request):
     names = request.get('name', None)
-    return '200 OK', render('about.html', names=names)
-def contact_view(request):
+    return '200 OK', render('teachers.html', names=names)
+def create_course(request):
     # Проверка метода запроса
     if request['method'] == 'POST':
         data = request['data']
         title = data['title']
         text = data['text']
         email = data['email']
-        print(f'Нам пришло сообщение от {email} с темой {title} и текстом {text}')
+        print(f'Нам пришло сообщение от {email} с просьбой создать курс {title} и текстом {text}')
         return '200 OK', render('contact_post.html', title=title, text=text, email=email)
     else:
         return '200 OK', render('contact.html')
