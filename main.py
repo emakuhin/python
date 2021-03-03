82de5533f5ad0f4dfa5a5690dd55dd07ed9ea4bf
e.makukhin <maku@mail.ru>
2021-03-03 14:29:26 +0300

    Lesson-6

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from core import Application
 from functions import render
 from wsgiref.simple_server import make_server
-from models import Logger, TrainingSite, debug
+from models import Logger, TrainingSite, debug, EmailNotifier, SmsNotifier, BaseSerializer
 
 
 
 logger = Logger('main')
 site = TrainingSite()
+email_notifier = EmailNotifier()
+sms_notifier = SmsNotifier()
 
 
 
@@ -57,6 +59,8 @@ def create_course(request):
         print('Пришол запрос', category)
         if category:
             course = site.create_course(name, site.find_category(category))
+            course.observers.append(email_notifier)
+            course.observers.append(sms_notifier)
             site.courses.append(course)
         return '200 OK', render('create_course.html')
     else:
@@ -74,6 +78,42 @@ def category_list(request):
     logger.log('Список категорий')
     return '200 OK', render('category_list.html', items=site.categories)
 
+@application.add_route('/create_student/')
+def create_student(request):
+    if request['method'] == 'POST':
+        data = request['data']
+        name = Application.decode_value(data['name'])
+        new_obj = site.create_user('student', name)
+        site.students.append(new_obj)
+    return '200 OK', render('create_student.html')
+
+@application.add_route('/students/')
+def student_list(request):
+    return '200 OK', render('student_list.html', students=site.students)
+
+
+@application.add_route('/add_student/')
+@debug
+def create_course(request):
+    if request['method'] == 'POST':
+        data = request['data']
+        student_name = data['student']
+        student = site.get_student(student_name)
+        course_name = data.get('courses')
+        course = site.get_course(course_name)
+        course.add_student(student)
+        print(f'Пришол запрос от {student} записаться на курс {course}')
+        courses = site.courses
+        students = site.students
+        return '200 OK', render('create_course.html', courses=courses, students=students)
+    else:
+        courses = site.courses
+        students = site.students
+        return '200 OK', render('add_student.html', courses=courses, students=students)
+
+@application.add_route('/api/')
+def course_api(request):
+    return '200 OK', BaseSerializer(site.courses).save()
 
 
 with make_server('', 8000, application) as httpd:
--- models.py
+++ models.py
@@ -1,11 +1,30 @@
 from singletone import SingletonByName
 import time
+import jsonpickle
+
+class User:
+    def __init__(self, name):
+        self.name = name
+
+class Teacher(User):
+    pass
+
+class Student(User):
+    def __init__(self, name):
+        self.courses = []
+        super().__init__(name)
+
 
 class TrainingSite:
     def __init__(self):
         self.teachers = ['alex', 'mak', 'sergei', 'Tim']
         self.courses = []
         self.categories = []
+        self.students = []
+
+    @staticmethod
+    def create_user(type_, name):
+        return UserFactory.create(type_, name)
 
     @staticmethod
     def create_category(name, category=None):
@@ -28,6 +47,10 @@ class TrainingSite:
                 return item
         return False
 
+    def get_student(self, name) -> Student:
+        for item in self.students:
+            if item.name == name:
+                return item
 
 
 class Category:
@@ -42,14 +65,49 @@ class Category:
         return result
 
 
+class Subject:
 
-class Course:
+    def __init__(self):
+        self.observers = []
+
+    def notify(self):
+        for item in self.observers:
+            item.update(self)
+
+class Observer:
+
+    def update(self, subject):
+        pass
+
+
+class Course(Subject):
 
     def __init__(self, name, category):
         self.name = name
         self.category = category
         self.category.courses.append(self)
+        self.students = []
+        super().__init__()
 
+    def __getitem__(self, item):
+        return self.students[item]
+
+    def add_student(self, student: Student):
+        self.students.append(student)
+        student.courses.append(self)
+        self.notify()
+
+
+class SmsNotifier(Observer):
+
+    def update(self, subject: Course):
+        print('SMS->', 'к нам присоединился', subject.students[-1].name)
+
+
+class EmailNotifier(Observer):
+
+    def update(self, subject: Course):
+        print(('EMAIL->', 'к нам присоединился', subject.students[-1].name))
 
 class Logger(metaclass=SingletonByName):
 
@@ -68,3 +126,27 @@ def debug(func):
         return result
     return inner
 
+
+
+
+class UserFactory:
+    types = {
+        'student': Student,
+        'teacher': Teacher
+    }
+
+    @classmethod
+    def create(cls, type_, name):
+        return cls.types[type_](name)
+
+
+class BaseSerializer:
+
+    def __init__(self, obj):
+        self.obj = obj
+
+    def save(self):
+        return jsonpickle.dumps(self.obj)
+
+    def load(self, data):
+        return jsonpickle.loads(data)
